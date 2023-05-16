30a1923f76dacf6fce6a92101dafce659a407589
zeeminkim <mjkim@humuson.com>
2023-05-16 13:43:33 +0900

    fix: 툴팁 컴포넌트 추가 보완

--- src/stories/Tooltip.stories.js
+++ src/stories/Tooltip.stories.js
@@ -13,3 +13,27 @@ export const Basic = {
         tooltipText: '툴팁 도움말입니다.'
     },
 };
+
+export const Right = {
+    args: {
+        label: '도움말',
+        position: 'right',
+        tooltipText: '툴팁 도움말입니다.'
+    },
+};
+
+export const Bottom = {
+    args: {
+        label: '도움말',
+        position: 'bottom',
+        tooltipText: '툴팁 도움말입니다.'
+    },
+};
+
+export const Left = {
+    args: {
+        label: '도움말',
+        position: 'left',
+        tooltipText: '툴팁 도움말입니다.'
+    },
+};
